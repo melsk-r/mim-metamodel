5fffec225936649852ee737effd13f7327675625
architolk <msj@xs4all.nl>
2025-03-31 12:57:05 +0200

    Completed libraries

--- werkomgeving/voorbeeldmodel/begrippenkader/convert.sh
+++ werkomgeving/voorbeeldmodel/begrippenkader/convert.sh
@@ -11,23 +11,23 @@ fi
 if [ ! -f "../../libs/rdf2xml.jar" ]; then
   curl -L -k https://github.com/architolk/rdf2xml/releases/download/v1.1.0/rdf2xml.jar -o ../../libs/rdf2xml.jar
 fi
-if [ ! -f "../../libs/excel2skos.yaml" ]; then
-  curl -L -k https://raw.githubusercontent.com/architolk/rdf2rdf/main/excel2skos.yaml -o ../../libs/excel2skos.yaml
+if [ ! -f "../../libs/csv2skos.yaml" ]; then
+  curl -L -k https://raw.githubusercontent.com/architolk/rdf2rdf/refs/heads/master/csv2skos.yaml -o ../../libs/csv2skos.yaml
 fi
 if [ ! -f "../../libs/skos2md.xsl" ]; then
-  curl -L -k https://raw.githubusercontent.com/architolk/rdf2xml/main/skos2md.xsl -o ../../libs/skos2md.xsl
+  curl -L -k https://raw.githubusercontent.com/architolk/rdf2xml/refs/head/main/skos2md.xsl -o ../../libs/skos2md.xsl
 fi
 if [ ! -f "../../libs/dct2md.xsl" ]; then
-  curl -L -k https://raw.githubusercontent.com/architolk/rdf2xml/main/dct2md.xsl -o ../../libs/dct2md.xsl
+  curl -L -k https://raw.githubusercontent.com/architolk/rdf2xml/refs/head/main/dct2md.xsl -o ../../libs/dct2md.xsl
 fi
 if [ ! -f "../../libs/skos2graphml.xsl" ]; then
-  curl -L -k https://raw.githubusercontent.com/architolk/rdf2xml/main/dct2md.xsl -o ../../libs/skos2graphml.xsl
+  curl -L -k https://raw.githubusercontent.com/architolk/rdf2xml/refs/head/main/dct2md.xsl -o ../../libs/skos2graphml.xsl
 fi
 
 # Convert: .xlsx to .ttl (csv in rdf)
 java -jar ../../libs/excel2rdf.jar Fietsenwinkel.xlsx fietsenwinkel-csv.ttl
 # Convert: csv in rdf to skos
-java -jar ../../libs/rdf2rdf.jar -i fietsenwinkel-csv.ttl -o fietsenwinkel-skos.ttl -c ../../libs/excel2skos.yaml
+java -jar ../../libs/rdf2rdf.jar -i fietsenwinkel-csv.ttl -o fietsenwinkel-skos.ttl -c ../../libs/csv2skos.yaml
 # - Publicatie
 java -jar ../../libs/rdf2xml.jar fietsenwinkel-skos.ttl fietsenwinkel-begrippenkader.md ../../libs/skos2md.xsl
 java -jar ../../libs/rdf2xml.jar fietsenwinkel-skos.ttl localbiblio.js ../../libs/dct2md.xsl
